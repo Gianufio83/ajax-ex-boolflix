88ce33a93d6c0e255c94c2ef12618140b9a6fdf0
Gianufio83 <fiore.gianuario@gmail.com>
2020-02-06 11:53:24 +0100

    inizio, ricerca API Key, settaggio parametri di ricerca, creazione input e button

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// 9870f35e71374469c3af0707aac57353 -API key-
+// https://api.themoviedb.org/3/search/movie?api_key=9870f35e71374469c3af0707aac57353&query=fight club&language=it-IT -chiamata completa-
+
+// Milestone 1:
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+// Titolo
+// Titolo Originale
+// Lingua
+// Voto
+$(document).ready(function () {
+  
+});
